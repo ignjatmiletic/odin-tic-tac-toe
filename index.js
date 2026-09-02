428358ce82bd5deb9ae038d186fda7341e8e1067
Ignjat <254181970+ignjatmiletic@users.noreply.github.com>
2026-09-02 14:42:12 +0200

    Dodao logiku za proveravanje dostupnih poteza

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 
+function GameBoard()
+{
+    const rows = 3;
+    const columns = 3;
+    const board = []
+
+    for(let i = 0; i < rows; i++)
+    {
+        board[i] = []
+        for(let j = 0; j < columns; j++)
+        {
+            board[i].push(Cell())
+        }
+    }
+
+
+    const getBoard = () => board
+
+
+    const placeSign = (column, row, player) => 
+    {
+        if (board[row][column].getValue() !== 0)
+            return;
+
+       board[row][column].addToken(player);
+    }
+    return{
+        placeSign,
+        getBoard
+    }
+}
+
+function Cell()
+{
+    let value = 0;
+
+    const getValue = () => value
+
+    return {
+    getValue
+  };
+}
+
+
+let game = GameBoard()
+game.placeSign(1,1,'kurac')
